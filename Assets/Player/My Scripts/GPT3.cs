82172b5a1cb3b4a2b9a398ed83b10dcec0929255
himanshuskyrockets <84278213+himanshuskyrockets@users.noreply.github.com>
2023-01-03 18:20:01 +0530

    error
    Error is not fixed in the Gpt3

--- Assets/Player/My Scripts/GPT3.cs
+++ Assets/Player/My Scripts/GPT3.cs
@@ -1,18 +1,46 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Networking;
+using System.Text;
 
 public class GPT3 : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public string apiKey = "OpenAI API Key";
+    // public string model;
+     private const string API_URL = "https://api.openai.com/v1/completions";
+    public string prompt="Hello, how are you today?";
+   public int maxTokens = 2048;
+    public float temperature = 0.7f;
+
     void Start()
     {
-        
+        StartCoroutine(SendGpt3Request());
     }
 
-    // Update is called once per frame
-    void Update()
+   IEnumerator SendGpt3Request()
+{
+     UnityWebRequest request = new UnityWebRequest(API_URL, "POST");
+       byte[] bodyRaw = Encoding.UTF8.GetBytes("{\"model\": \"text-curie-001\", \"prompt\": \"" + prompt + "\", \"max_tokens\": " + maxTokens + ", \"temperature\": " + temperature + "}");
+        request.uploadHandler = new UploadHandlerRaw(bodyRaw);
+        request.downloadHandler = new DownloadHandlerBuffer();
+        request.SetRequestHeader("Content-Type", "application/json");
+        request.SetRequestHeader("Authorization", "Bearer " + apiKey);
+ 
+    // Send the request and wait for the response
+    yield return request.SendWebRequest();
+
+    // Check for errors
+    if (request.result == UnityWebRequest.Result.ConnectionError || request.result == UnityWebRequest.Result.ProtocolError || request.result == UnityWebRequest.Result.DataProcessingError)
     {
-        
+        Debug.LogError(request.error);
+    }
+    else
+    {
+        // Get the response text
+        string responseText = request.downloadHandler.text;
+        Debug.Log(responseText);
     }
 }
+
+}//class
